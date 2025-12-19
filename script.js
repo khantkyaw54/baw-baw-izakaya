30e9a6c7ea17608563beef97d1c249a788d76b70
khantkyaw54 <khantkyaw7291@gmail.com>
2025-12-19 23:36:03 +0900

    changes

--- script.js
+++ script.js
@@ -14,6 +14,25 @@ hamburger.addEventListener("click", () => {
     gnav.classList.toggle("active");
 });
 
+// Back to Top Buttonß
+    const backToTop = document.getElementById('backToTop');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+        backToTop.classList.add('show');
+  } else {
+        backToTop.classList.remove('show');
+  }
+});
+
+backToTop.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+});
+
+
 
 
 document.addEventListener("DOMContentLoaded", () => {
